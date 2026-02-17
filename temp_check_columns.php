11f82cc696c0999d034fcc95411d315680e4865a
Seu Nome <seuemail@gmail.com>
2026-02-17 15:12:18 -0300

    clean: remove arquivos temporários de debug
    - Remove todos os arquivos temp_*.php criados durante debug
    - Mantém backup do Controller como referência
    - Limpeza do working directory

--- temp_check_columns.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=r225us.hmservers.net;port=3306;dbname=pixel12digital_pixelhub;charset=utf8mb4', 'pixel12digital_pixelhub', 'Los@ngo#081081');
-$stmt = $pdo->query('DESCRIBE communication_events');
-echo "Colunas da tabela communication_events:\n";
-while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-    echo $row[0] . ' - ' . $row[1] . "\n";
-}
