c09134a36befe295d893aca61888bdbe0145da0c
Seu Nome <seuemail@gmail.com>
2026-02-21 09:19:42 -0300

    fix: commita vendor/composer autoload para resolver erro 500 no servidor sem composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pixel12/pixel-hub',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6b397eddb813855943735be84f3b0665ba940aa2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.2',
+            'version' => '7.0.2.0',
+            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pixel12/pixel-hub' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6b397eddb813855943735be84f3b0665ba940aa2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
