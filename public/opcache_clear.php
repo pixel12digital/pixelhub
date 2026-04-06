b3dbe59316b4931ac888d5785f4ec694980d6e9e
Seu Nome <seuemail@gmail.com>
2026-04-06 09:54:05 -0300

    chore: remove opcache_clear.php temp file

--- public/opcache_clear.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo json_encode(['status' => 'opcache cleared']);
-} else {
-    echo json_encode(['status' => 'opcache not available']);
-}
